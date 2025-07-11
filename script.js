a004ab07788463f2900aa9f8218d904a55cef9b9
icyscriptzz <scriptsdoicylgl@gmail.com>
2025-07-11 10:07:26 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Define o link da imagem da Corrida do Abidala quando você tiver ele
+document.addEventListener("DOMContentLoaded", () => {
+  const corridaImg = document.getElementById("img-corrida");
+
+  // Substitua esse link pela imagem real da Corrida do Abidala quando você tiver
+  corridaImg.src = "https://via.placeholder.com/600x300.png?text=Corrida+do+Abidala"; 
+});
